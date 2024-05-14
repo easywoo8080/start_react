8e45a115d498eb97f801598cce1b371e68598077
jwMoon <jwmoon@nivuskorea.com>
2024-05-14 10:53:02 +0900

    리액트 스타트

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './TIL/useEffect02/App';
+import App from './TIL/0309/db/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
